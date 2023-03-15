9458d2c8e58c6155863da2eb53a19b366ab8747e
cristiano.moraes <cristiano.moraes@ituran.com.br>
2023-03-14 23:02:52 -0300

    Rotation Implemented

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
-    
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -31,6 +31,10 @@ public class Player : MonoBehaviour
         
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        transform.position += moveDir * moveSpeed * Time.deltaTime; 
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float rotateSpeed = 10f;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
