4230a5fb63bc3718adb6f805de21f5c8de91d0a0
cristiano.moraes <cristiano.moraes@ituran.com.br>
2023-05-02 01:43:42 -0300

    Refactor to the new input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,33 +6,19 @@ public class Player : MonoBehaviour
 {
     [SerializeField] 
     private float moveSpeed = 7f;
+    [SerializeField] 
+    private GameInput gameInput;
     private bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
+        Vector2 inputVector = gameInput.GetInputVectorNormalized();
 
-        inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        isWalking = moveDir != Vector3.zero;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+        
+        isWalking = moveDir != Vector3.zero;
+        
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
