c37de453d122df40ab56b4901ab1d009b109a85e
cristiano.moraes <cristiano.moraes@ituran.com.br>
2023-05-03 15:28:22 -0300

    Add clear counter prefab and iteraction layer to the project

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,50 @@ public class Player : MonoBehaviour
     private float moveSpeed = 7f;
     [SerializeField] 
     private GameInput gameInput;
+    [SerializeField]
+    private LayerMask countersLayerMask;
     private bool isWalking;
+    private Vector3 lastInteractDir;
 
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetInputVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if(moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        } 
+        else
+        {
+            Debug.Log("-");
+        }
+
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetInputVectorNormalized();
 
@@ -60,10 +101,5 @@ public class Player : MonoBehaviour
         
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 }
