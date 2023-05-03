fa229f4af985d713ea873f95a9c84cf9b81e224f
cristiano.moraes <cristiano.moraes@ituran.com.br>
2023-05-02 21:01:06 -0300

    Implementation of colisions on player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,7 +13,48 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetInputVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if(!canMove)
+        {
+            // Cant move towards moveDir
+
+            // Atempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove)
+            {
+                // Can move only on the x
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                
+                if(canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // Cannot move in any direction
+                }
+
+            }
+
+        }
+
+        if(canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
         
         isWalking = moveDir != Vector3.zero;
         
